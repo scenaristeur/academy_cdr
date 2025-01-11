3207647328b646814e470d18bb84e111d80c0dfb
scenaristeur <scenaristeur@gmail.com>
2025-01-11 15:18:13 +0100

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,43 @@
 import { fileURLToPath, URL } from 'node:url'
-
+const publicPath = process.env.NODE_ENV === 'production' ? '/ypu/' : '/'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  publicPath: publicPath,
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  pwa: {
+    name: 'CDR Academie',
+    // themeColor: '#4dbab5',
+    // msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    manifestOptions: {
+      share_target: {
+        action: publicPath + 'share',
+        method: 'GET',
+        enctype: 'application/x-www-form-urlencoded',
+        params: {
+          title: 'title',
+          text: 'text',
+          url: 'url',
+        },
+      },
+    },
+
+    // configure the workbox plugin
+    /*  workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+    // swSrc is required in InjectManifest mode.
+    swSrc: 'src/registerServiceWorker.js',
+    // ...other Workbox options...
+  }*/
   },
 })
